73a32ab09226259ad9f33a6a98ed688acbbf1820
marcoma <www.ma.net@gmail.com>
2015-03-24 22:00:13 +0800

    Add command support for test.js

--- test.js
+++ test.js
@@ -1,18 +1,30 @@
+#!/usr/bin/env node
+
 var fivebeans =require('fivebeans');
 var uuid = require('node-uuid');
 
-var client = new fivebeans.client('challenge.aftership.net', 11300);
+var argv = require('yargs')
+    .usage('Usage: node test.js --host [URL] --port [port] --tube [tube_id] --type [job_type] --from [from_currency] --to [to_currency]')
+    .default('host', 'challenge.aftership.net')
+    .default('port', '11300')
+    .default('tube', 'marcohk')
+    .default('type', 'currency-convert')
+    .default('from', 'USD')
+    .default('to', 'HKD')
+    .argv;
+
+var client = new fivebeans.client(argv.host, parseInt(argv.port));
 
 console.log(Date.now());
 
 var job =
 {
-    type: 'currency-convert',
+    type: argv.type,
     payload:
     {
         id: uuid.v1(), 
-        from: 'USD', 
-        to: 'EUR'
+        from: argv.from, 
+        to: argv.to
     }
 };
 
@@ -21,12 +33,12 @@ client
     {
         // client can now be used
         console.log('connect');
-        client.use('marcohk', function(err, tname) {
+        client.use(argv.tube, function(err, tname) {
             console.log("using " + tname);
 
-            client.put(0, 0, 60, JSON.stringify(['currency-convert',job]), function(err, jobid)
+            client.put(0, 0, 60, JSON.stringify([argv.type,job]), function(err, jobid)
             {
-                console.log('queued a string reverse job in marcohk: ' + jobid);
+                console.log('queued a string reverse job in ' + argv.tube + ': ' + jobid);
             });
         });
     })
